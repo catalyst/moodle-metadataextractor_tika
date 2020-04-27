af41c9209f880cb31c2fc275637fe09c9f549b81
Tom Dickman <tomdickman@catalyst-au.net>
2020-04-27 13:23:32 +1000

    enhancement: add supplementary metadata for url resources

--- tests/extractor_test.php
+++ tests/extractor_test.php
@@ -22,7 +22,6 @@
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 use metadataextractor_tika\extractor;
-use metadataextractor_tika\tika_helper;
 
 defined('MOODLE_INTERNAL') || die();
 
